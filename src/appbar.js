f9b91d2e33ca456df2e09e84a206a3b75a854159
AhmadMtr <70106976+Ahmad-Mtr@users.noreply.github.com>
2024-05-24 21:25:26 +0300

    finish Desktop-UI (#3)
    * add appBar buttons
    
    * finish Desktop UI
    
    * AhmadMtr/finish-Desktop-UI

--- src/appbar.js
+++ src/appbar.js
@@ -3,7 +3,8 @@ import {
   svgAppStore,
   svgCode,
   svgGJU,
-  svgKrunker,
+  svgCalculator,
+  svgWeather,
   svgLichess,
   svgNotepad,
   svgPhotopea,
@@ -12,42 +13,47 @@ export function createAppBar() {
   const appBar = document.getElementById("app-bar");
 
   const appIconAppStore = document.createElement("div");
-  appIconAppStore.className = "app-icon-app-store";
+  appIconAppStore.className = "app-icon";
   appIconAppStore.innerHTML = svgAppStore;
   appBar.appendChild(appIconAppStore);
 
   const appIconNotepad = document.createElement("div");
-  appIconNotepad.className = "app-icon-notepad";
-  appIconNotepad.innerHTML =svgNotepad;
+  appIconNotepad.className = "app-icon";
+  appIconNotepad.innerHTML = svgNotepad;
   appBar.appendChild(appIconNotepad);
 
+  const appIconCalculator = document.createElement("div");
+  appIconCalculator.className = "app-icon";
+  appIconCalculator.innerHTML = svgCalculator;
+  appBar.appendChild(appIconCalculator);
+
+  const appIconWeather = document.createElement("div");
+  appIconWeather.className = "app-icon";
+  appIconWeather.innerHTML = svgWeather;
+  appBar.appendChild(appIconWeather);
+
+  const appIconLichess = document.createElement("div");
+  appIconLichess.className = "app-icon";
+  appIconLichess.innerHTML = svgLichess;
+  appBar.appendChild(appIconLichess);
+
+  const appIconMyGJU = document.createElement("div");
+  appIconMyGJU.className = "app-icon";
+  appIconMyGJU.innerHTML = svgMyGJU;
+  appBar.appendChild(appIconMyGJU);
+
+  const appIconGju = document.createElement("div");
+  appIconGju.className = "app-icon";
+  appIconGju.innerHTML = svgGJU;
+  appBar.appendChild(appIconGju);
+
   const appIconVscode = document.createElement("div");
-  appIconVscode.className = "app-icon-vscode";
+  appIconVscode.className = "app-icon";
   appIconVscode.innerHTML = svgCode;
   appBar.appendChild(appIconVscode);
 
   const appIconPhotopea = document.createElement("div");
-  appIconPhotopea.className = "app-icon-photopea";
+  appIconPhotopea.className = "app-icon";
   appIconPhotopea.innerHTML = svgPhotopea;
   appBar.appendChild(appIconPhotopea);
-
-  const appIconGju = document.createElement("div");
-  appIconGju.className = "app-icon-gju";
-  appIconGju.innerHTML = svgGJU;
-  appBar.appendChild(appIconGju);
-
-  const appIconMyGJU = document.createElement("div");
-  appIconMyGJU.className = "app-icon-mygju";
-  appIconMyGJU.innerHTML = svgMyGJU;
-  appBar.appendChild(appIconMyGJU);
-
-  const appIconLichess = document.createElement("div");
-  appIconLichess.className = "app-icon-lichess";
-  appIconLichess.innerHTML = svgLichess;
-  appBar.appendChild(appIconLichess);
-
-  const appIconKrunker = document.createElement("div");
-  appIconKrunker.className = "app-icon-krunker";
-  appIconKrunker.innerHTML = svgKrunker;
-  appBar.appendChild(appIconKrunker);
 }
--- src/index.js
+++ src/index.js
@@ -1,62 +1,128 @@
 //import { initOS } from "./init_os";
 import { saveUserAccount } from "./user/user_api";
 
-
-import { createAppBar } from './appbar';
-import { createTaskBar } from './taskbar';
-import { WindowManager } from './window_manager';
-
-document.addEventListener('DOMContentLoaded', () => {
+import { createAppBar } from "./appbar";
+import { createTaskBar } from "./taskbar";
+import { WindowManager } from "./window_manager";
+import {
+  getAccountPath,
+  getAppStorePath,
+  getCalculatorPath,
+  getFileExplorerPath,
+  getGJUPath,
+  getLichessPath,
+  getMyGJUPath,
+  getNotePadPath,
+  getPhotopeaPath,
+  getTerminalPath,
+  getVSCodePath,
+  getWeatherAppPath,
+} from "./apps-list";
+document.addEventListener("DOMContentLoaded", () => {
   createAppBar();
   createTaskBar();
   const windowManager = new WindowManager();
   // windowManager.createWindow('MyGJU', 'https://mygju.gju.edu.jo/faces/index.xhtml');
 
-  document.addEventListener('keydown', (event) => {
-    if (event.ctrlKey && event.key === 'y') {
-      const shell = document.querySelector('.shell');
-      shell.classList.toggle('hidden');
+  document.addEventListener("keydown", (event) => {
+    if (event.ctrlKey && event.key === "y") {
+      windowManager.createWindow("Terminal", getTerminalPath);
     }
   });
-  
-  document.querySelectorAll('.task-icon').forEach((icon, index) => {
-    icon.addEventListener('click', () => {
-      console.log('✨✨✨✨');
-      windowManager.createWindow(`MyGJU`, '../src/apps/mygju/mygju.html');
-    });
+
+  // appBar Buttons
+  document.querySelectorAll(".app-icon").forEach((icon, index) => {
+    clickAppButton(windowManager, index, icon);
   });
-  
-  document.querySelectorAll('.app-icon-mygju').forEach((icon, index) => {
-    icon.addEventListener('click', () => {
-      console.log('📄📄📄📄📄📄📄');
-      windowManager.createWindow(`MyGJU`, '../src/apps/mygju/mygju.html');
-    });
-  });
-  
-  document.querySelectorAll('.app-icon-photopea').forEach((icon, index) => {
-    icon.addEventListener('click', () => {
-      console.log('📄📄📄📄📄📄📄');
-      windowManager.createWindow(`Photopea`, `https://photopea.com`);
-    });
-  });
-  
-  document.querySelectorAll('.app-icon-notepad').forEach((icon, index) => {
-    icon.addEventListener('click', () => {
-      windowManager.createWindow(`Notepad`, `../src/apps/notepad/index.html`);
-    });
+  // taskBar Icon Buttons
+  document.querySelectorAll(".task-icon").forEach((icon, index) => {
+    clickTaskButton(windowManager, index, icon);
   });
 });
 
-//await initOS();
+export function clickAppButton(windowManager, index, icon) {
+  switch (index) {
+    case 0:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`AppStore`, getAppStorePath);
+      });
+      break;
+    case 1:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`NotePad`, getNotePadPath);
+      });
+      break;
+    case 2:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`Calculator`, getCalculatorPath);
+      });
+      break;
+    case 3:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`Weather`, getWeatherAppPath);
+      });
+      break;
+    case 4:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`Lichess`, getLichessPath);
+      });
+      break;
+    case 5:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`MyGJU`, getMyGJUPath);
+      });
+      break;
+    case 6:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`GJU`, getGJUPath);
+      });
+      break;
+    case 7:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`Code`, getVSCodePath);
+      });
+      break;
+    case 8:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`Photo Editor`, getPhotopeaPath);
+      });
+      break;
 
+    default:
+      break;
+  }
+}
+export function clickTaskButton(windowManager, index, icon) {
+  switch (index) {
+    case 0:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`File Explorer`, getFileExplorerPath);
+      });
+      break;
+    case 1:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`Terminal`, getTerminalPath);
+      });
+      break;
+    case 2:
+      icon.addEventListener("click", () => {
+        windowManager.createWindow(`Account`, getAccountPath);
+      });
+      break;
+
+    default:
+      break;
+  }
+}
+//await initOS();
 
 // @toBeDeleted later
 // document.getElementById('loginForm').addEventListener('submit', function(event) {
 //     event.preventDefault(); // Prevent form submission
 //     const username = document.getElementById('username').value;
 //     const password = document.getElementById('password').value;
-  
+
 //     // # From User API
 //     saveUserAccount(username, password);
-    
+
 // });
